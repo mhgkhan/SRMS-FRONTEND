86e692852a1f4510a24dd5c324936871d771d55d
Muhammad Hasnain <muhammadhasnainghazna@gmail.com>
2024-07-30 20:01:22 +0500

    Added Footer

--- src/main.jsx
+++ src/main.jsx
@@ -3,12 +3,14 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 import Header from '../components/Header.jsx'
+import Footer from '../components/Footer.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <main>
       <Header />
       <App />
+      <Footer />
     </main>
   </React.StrictMode>,
 )
