b1da9a3b0d1c5dc302f77dcb3e33905961ae60f9
Muhammad Hasnain <muhammadhasnainghazna@gmail.com>
2024-07-30 21:30:39 +0500

    Created Pages, And Add on the route

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,25 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom"
+import Header from "../components/Header"
+import Footer from "../components/Footer"
+import Home from "./pages/Home"
+import Login from "./pages/Login"
+import Profile from "./pages/profile/Profile"
+import Signup from "./pages/Signup"
 
 const App = () => {
-
-
   return (
-    <div>
-      hello world 
-    </div>
+    <BrowserRouter>
+      <main>
+        <Header />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/profile" element={<Profile />} />
+          <Route path="/signup" element={<Signup />} />
+        </Routes>
+        <Footer />
+      </main>
+    </BrowserRouter>
   )
 }
 
