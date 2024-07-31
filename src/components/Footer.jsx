ec7e11dcc8c2ad291df05ab915c3d62186a4b02f
Muhammad Hasnain <muhammadhasnainghazna@gmail.com>
2024-07-31 12:46:56 +0500

    Footer Created

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,7 +2,11 @@ import React from 'react'
 
 const Footer = () => {
   return (
-    <footer>Footer</footer>
+    <footer className='w-full bg-gray-700'>
+      <div className="container mx-auto">
+        <div className="text-center text-lg text-white">Copyright&copy; allright reserved 2024 <a href="https://ghaznaprogrammar.com/">Ghazna Programmar</a> </div>
+      </div>
+    </footer>
   )
 }
 
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,14 +1,36 @@
-import React from 'react'
+import { Link } from 'react-router-dom'
 
 const Header = () => {
   return (
-    <header className='bg-blue-300'>
+    <header className='bg-gray-700 h-auto'>
       <div className="container mx-auto">
-        <div className="flex items-end justify-between gap-3">
-          <div className="logo">
-            <span>GH</span>
-            <span>SRMS</span>
+        <div className="flex md:flex-row flex-col md:items-end items-center justify-between ">
+
+
+          <div className="logo px-2 pt-2 flex gap-[1px]">
+            <span className='bg-white text-2xl text-gray-700 font-bold  border-2 border-gray-600 px-2 pt-1  rounded-md'>GH</span>
+            <span className='bg-white text-2xl text-gray-700 italic  border-2 border-gray-600 px-2 pt-1  rounded-md'>SRMS</span>
           </div>
+
+
+
+        <nav className='md:mt-0 mt-3'>
+          <ul className='flex items-end justify-center gap-2'>
+
+            <li>
+              <Link className='text-white p-2 font-bold hover:bg-white hover:text-black rounded-lg transition-all duration-500' to={'/'}>HOME</Link>
+            </li>
+            <li>
+              <Link className='text-white p-2 font-bold hover:bg-white hover:text-black rounded-lg transition-all duration-500' to={'/studentresult'}>RESULT</Link>
+            </li>
+            <li>
+              <Link className='text-white p-2 font-bold hover:bg-white hover:text-black rounded-lg transition-all duration-500' to={'/signup'}>LOGIN</Link>
+            </li>
+          </ul>
+        </nav>
+
+
+
         </div>
       </div>
     </header>
