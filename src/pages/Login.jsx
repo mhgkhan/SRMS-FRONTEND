e397e552f23e7c99a3de977d2d6a26a6465ec664
Muhammad Hasnain <muhammadhasnainghazna@gmail.com>
2024-08-02 12:20:32 +0500

    Added login submit form with error handling and response rendering

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,4 +1,7 @@
 import { useState } from 'react'
+import {ApiDomain} from "./../config/DomainUrls"
+import toast, { Toaster } from 'react-hot-toast'
+
 
 
 const Login = () => {
@@ -41,13 +44,64 @@ const Login = () => {
     setInputs({ ...inputs, [e.target.name]: e.target.value })
   }
 
+
+
+  // states for send request and recieve response 
+  const [loading,setLoading] = useState(false)
+
+  const [isResErr,setIsResErr] = useState(false);
+  const [resErrMsg,setResErrMsg] = useState("")
+
+  const [recieveData,setRecieveData] = useState({})
+
+
+
   const submitForm = async (e) => {
     e.preventDefault()
     const checkingValidate = checkValidate();
 
     if (checkingValidate) {
-      console.log("data are valid")
-      console.log(inputs)
+      // console.log("data are valid")
+      // console.log(inputs)
+    
+      try {
+        setLoading(true)
+
+        // calling to the api 
+  
+        const reqAndRes = await (await fetch(`${ApiDomain}appgh/auth/student/signin`,{
+          method:"POST",
+          headers:{'content-type':'application/json'},
+          body: JSON.stringify({stdId:inputs.studentId, stdPassword: inputs.studentPassword})
+        })).json();
+  
+        setLoading(false)
+  
+        if(reqAndRes.success){
+          setIsResErr(false)
+          setRecieveData(reqAndRes.data)
+          toast.success("Logged sucessfully...",{
+            icon:"🎉"
+          })
+          setInputs({studentId:"", studentPassword:""})
+        }
+        else{
+          setIsResErr(true)
+          setResErrMsg(reqAndRes.message)
+          toast.error(reqAndRes.message,{
+            icon:"😒"
+          })
+        }
+      } catch (error) {
+        setLoading(false)
+        setIsResErr(true)
+          setResErrMsg("Some error occured")
+          toast.error("Some error occured",{
+            icon:"😒"
+          })
+      }
+
+
     }
     else{
       console.log("data are not valid")
@@ -61,6 +115,7 @@ const Login = () => {
 
   return (
     <main className='min-h-screen bg-gray-100'>
+      <Toaster position='top-right' reverseOrder={false} />
       <div className="container mx-auto flex items-center justify-center">
 
         <div className="md:w-[70%] mt-10 w-[90%] rounded-md shadow-md shadow-gray-300 mx-auto bg-white">
@@ -79,16 +134,17 @@ const Login = () => {
 
 
           <form action='#' className='mt-3' onSubmit={submitForm} method='post'>
-            <input onChange={changeLoginInputs} className='md:w-[70%] w-[95%] text-lg  block mx-auto border border-gray-700 p-2 m-2 rounded-md outline-none text-gray-700' type="text" name="studentId" placeholder='Student Id' required />
+            <input disabled={loading}  onChange={changeLoginInputs} className='  disabled:bg-gray-200  md:w-[70%] w-[95%] text-lg  block mx-auto border border-gray-700 p-2 m-2 rounded-md outline-none text-gray-700' type="text" name="studentId" placeholder='Student Id' required />
             {stdIdErrors.error ? <div className="text-red-800 w-[95%] text-xs mt-[-5px] md:w-[70%] mx-auto">{stdIdErrors.message}</div> : ""}
-            <input onChange={changeLoginInputs} className='md:w-[70%] w-[95%] text-lg  block mx-auto border border-gray-700 p-2 m-2 rounded-md outline-none text-gray-700' type="password" name='studentPassword' placeholder='Password' required />
+            <input  disabled={loading} onChange={changeLoginInputs} className=  ' disabled:bg-gray-200 md:w-[70%] w-[95%] text-lg  block mx-auto border border-gray-700 p-2 m-2 rounded-md outline-none text-gray-700' type="password" name='studentPassword' placeholder='Password' required />
             {stdPassErrors.error ? <div className="text-red-800 w-[95%] text-xs mt-[-5px] md:w-[70%] mx-auto">{stdPassErrors.message}</div> : ""}
-            <button disabled={inputs.studentId.length<1}  type="submit" className='disabled:bg-gray-600 hover:bg-gray-100 hover:text-gray-950 transition-all duration-300 text-lg text-center w-auto p-2 border border-gray-700 mt-4 rounded-md outline-none bg-gray-700 text-white block mx-auto'>Login</button>
+            <button disabled={inputs.studentId.length<1 || loading}  type="submit" className='disabled:bg-gray-600 hover:bg-gray-100 hover:text-gray-950 transition-all duration-300 text-lg text-center flex items-center justify-center gap-2 w-auto p-2 border border-gray-700 mt-4 rounded-md outline-none bg-gray-700 text-white block mx-auto'> {
+              loading? <span className="mx-auto w-[20px] h-[20px] border border-2 transition-all duration-300 animate-spin border-white rounded-full flex items-center justify-center text-white bg-black text-sm">W</span>: ""} Login</button>
           </form>
 
           <div className="head w-full bg-gray-700 px-3 py-2 rounded-md mt-5">
             <div className="links w-full text-center flex items-start justify-center gap-2 flex-wrap ">
-              <p className='text-white'>Not have an account <a className='text-blue-300' href="#">Signup</a> </p>
+              {/* <p className='text-white'>Not have an account <a className='text-blue-300' href="#">Signup</a> </p> */}
               <p className='text-white'>Forget password <a className='text-blue-300' href="#">Click here </a> </p>
             </div>
           </div>
